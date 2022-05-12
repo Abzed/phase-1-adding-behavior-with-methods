5e6c2ad1310979dba096d6629e9621aadd6849bb
Abzed <ibraabzed@gmail.com>
2022-05-12 11:45:55 +0300

    commit tree 88e028973d7bf2ccc12b2fb59a7e2c622028bc93

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+class Cat{
+    constructor(name, gender){
+        this.name = name,
+        this.gender = gender
+    }
+    speak(){
+        return `${this.name} says meow!`
+    }
+}
+
+const cat = new Cat("Sasha", "female")
+
+
+
+class Dog{
+    constructor(name, gender){
+        this.name = name,
+        this.gender = gender
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+}
+
+const dog = new Dog("Rufio", "male")
+ 
+
+
+class Bird{
+    constructor(name, gender){
+        this.name = name,
+        this.gender = gender
+    }
+    speak(){
+        if (this.name == 'Pablo'){
+            return `It's me! ${this.name}, the parrot!`
+        }else{
+            // console.log(`It's me! ${this.name}, the parrot!`);
+            // console.log(`${this.name} says squawk!`);
+            return `${this.name} says squawk!`
+        }
+    }
+}
+
+const bird = new Bird("Pablo", "male")
+const bird2 = new Bird("Mable", "female")
